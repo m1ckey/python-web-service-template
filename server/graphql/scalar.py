0f6f79a6a09f0ad234d1b3e47076134e2b692bbc
m1ckey <m1ckey@m1ckey.net>
2021-10-16 17:08:45 +0200

    add gql scalars

--- server/graphql/scalar.py
+++ server/graphql/scalar.py
@@ -1,9 +1,11 @@
 from base64 import b64decode, b64encode
 from datetime import datetime
+from uuid import UUID
 
 from ariadne import ScalarType
 
-base64 = ScalarType('Base64', serializer=lambda v: b64encode(v), value_parser=lambda v: b64decode(v))
-dt = ScalarType('DateTime', serializer=lambda v: v.isoformat(v), value_parser=lambda v: datetime.fromisoformat(v))
+base64 = ScalarType('Base64', serializer=lambda x: b64encode(x), value_parser=lambda x: b64decode(x))
+dt = ScalarType('DateTime', serializer=lambda x: x.isoformat(x), value_parser=lambda x: datetime.fromisoformat(x))
+uuid = ScalarType('UUID', serializer=lambda x: str(x), value_parser=lambda x: UUID(x))
 
-scalars = [base64, dt]
+scalars = [base64, dt, uuid]

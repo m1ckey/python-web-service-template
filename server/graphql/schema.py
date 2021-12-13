a07f3b9fbe501c644c5f5e99e63455de0a35f2a2
m1ckey <m1ckey@m1ckey.net>
2021-12-13 14:20:23 +0100

    edit graphql schema

--- server/graphql/schema.py
+++ server/graphql/schema.py
@@ -2,10 +2,12 @@ from ariadne import make_executable_schema, load_schema_from_path
 from ariadne.asgi import GraphQL
 
 from config import conf, ConfigProfile
+from .enum import enums
 from .mutation import mutation
 from .query import query
+from .resolver import types
 from .scalar import scalars
 
 type_defs = load_schema_from_path('server/graphql/schema.graphql')
-schema = make_executable_schema(type_defs, query, mutation, *scalars)
+schema = make_executable_schema(type_defs, query, mutation, *types, *scalars, *enums)
 app = GraphQL(schema, debug=conf.profile != ConfigProfile.PROD)

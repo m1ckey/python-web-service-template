eb5098f066781990d4c380916b36a53f855efa61
m1ckey <m1ckey@m1ckey.net>
2022-03-18 01:40:33 +0100

    add cors middleware

--- server/middleware.py
+++ server/middleware.py
@@ -1,8 +1,10 @@
 from starlette.middleware import Middleware
 from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from .auth import JWTAuth
 
 middleware = [
-    Middleware(AuthenticationMiddleware, backend=JWTAuth())
+    Middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*']),  # todo
+    Middleware(AuthenticationMiddleware, backend=JWTAuth()),
 ]
